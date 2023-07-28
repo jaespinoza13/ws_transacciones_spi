1a6084f5a95d793572c26334c14ad60897dd9fbc
caperez1 <caperez@mego.net>
2023-07-28 10:43:49 -0500

    fix: Actualizar el listado de las ifis

--- src/Domain/Entities/Consultas/EntidadFinanciera.cs
+++ src/Domain/Entities/Consultas/EntidadFinanciera.cs
@@ -6,4 +6,7 @@ public class EntidadFinanciera
     public string str_nombre_ifi { get; set; } = string.Empty;
     public string str_tipo { get; set; } = string.Empty;
     public string str_url_logo_ifi { get; set; } = string.Empty;
+    public int int_cod_ifi_autorizador { get; set; }
+    public string str_bin_receptor { get; set; } = string.Empty;
+    public string str_cuenta_bce { get; set; } = string.Empty;
 }
